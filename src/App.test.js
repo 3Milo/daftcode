1585429610f947c80007a7e99062232be6b3160b
Emil <emil@level7.systems>
2021-04-05 17:34:49 +0200

    fix app component test - render it within store provider

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { Provider } from 'react-redux';
+
+import store from './store/Store';
+
+describe('App Component Test', () => {
+  it("renders", () => {
+    render(<Provider store={store}><App /></Provider>);
+  });
 });
